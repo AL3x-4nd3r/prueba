509a6abecb387fedccee865f3a3e35496381ac53
AL3x-4nd3r <125616257+AL3x-4nd3r@users.noreply.github.com>
2023-02-21 23:14:01 +0100

    hola

---
+++ Github/src/token_prueba/MainToken.java
@@ -0,0 +1,12 @@
+package token_prueba;
+
+public class MainToken {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("hola mundo");
+	}
+
+}
